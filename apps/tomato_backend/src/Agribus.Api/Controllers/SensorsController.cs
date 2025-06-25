6954d85be326326d15871172503fa77cde92011c
Louisan Tchitoula <t.louisan@outlook.fr>
2025-06-25 15:12:01 +0200

    feat: add sensor data entities and controller for /data push endpoint

--- apps/tomato_backend/src/Agribus.Api/Controllers/SensorsController.cs
+++ apps/tomato_backend/src/Agribus.Api/Controllers/SensorsController.cs
@@ -1,4 +1,5 @@
 using Agribus.Core.Ports.Api.DTOs;
+using Agribus.Core.Tests;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Agribus.Api.Controllers;
@@ -8,21 +9,26 @@ namespace Agribus.Api.Controllers;
 public class SensorsController : ControllerBase
 {
     private readonly ILogger<SensorsController> _logger;
+    private readonly IParseSensorData _parseSensorData;
 
-    public SensorsController(ILogger<SensorsController> logger)
+    public SensorsController(ILogger<SensorsController> logger, IParseSensorData parseSensorData)
     {
         _logger = logger;
+        _parseSensorData = parseSensorData;
         // TODO: add influxdb infra
     }
 
     [HttpPost("/data")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public Task<IActionResult> PostSensorData([FromBody] RawSensorPayload payload)
+    public async Task<IActionResult> PostSensorData(
+        [FromBody] RawSensorPayload payload,
+        CancellationToken cancellationToken
+    )
     {
-        // TODO: parse
+        _logger.LogInformation("Received RawSensorPayload: {@Payload}", payload);
+        await _parseSensorData.FromRawJson(payload, cancellationToken);
         // TODO: push in influxdb
-        _logger.LogInformation("Received RawSensorPayload", payload);
-        return Task.FromResult<IActionResult>(Ok(payload));
+        return Created();
     }
 }
