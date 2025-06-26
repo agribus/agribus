2eb282ad9b5b9d6e5faabc4c1aa8b5d9ac7b141e
heitzjulien <contact@heitzjulien.fr>
2025-06-26 15:52:23 +0200

    refactor(scripts): replace git pull scripts with update_preprod logic

--- scripts/git_pull_preprod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="/usr/bin:/bin:/usr/local/bin"
-
-cd /srv/agribus || exit 1
-
-git checkout preprod
-git pull origin preprod
--- scripts/git_pull_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="/usr/bin:/bin:/usr/local/bin"
-
-cd /srv/agribus || exit 1
-
-git checkout main
-git pull origin main
