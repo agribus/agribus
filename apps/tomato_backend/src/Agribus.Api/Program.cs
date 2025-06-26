12c27f84271982a25b794e7e36dbf0e5a2868075
Louisan Tchitoula <t.louisan@outlook.fr>
2025-06-26 13:29:06 +0200

    feat(docker): add dev dockerfile and compose service

--- apps/tomato_backend/src/Agribus.Api/Program.cs
+++ apps/tomato_backend/src/Agribus.Api/Program.cs
@@ -6,20 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 builder.Services.AddPresentation().AddFeatures().AddInfrastructure();
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenAnyIP(5001); // <-- accepte les connexions externes
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
 
 
 var app = builder.Build();
