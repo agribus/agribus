0d002c189f848966881317ecf71f999576e3bbd0
Louisan Tchitoula <t.louisan@outlook.fr>
2025-06-27 23:33:00 +0200

    feat(config): structure the frontend monorepo

---
+++ apps/lychee_client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lychee.app',
+  appName: 'lychee',
+  webDir: 'dist/lychee_client/browser'
+};
+
+export default config;
