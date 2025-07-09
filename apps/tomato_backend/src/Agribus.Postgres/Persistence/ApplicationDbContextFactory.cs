bb0ed19d4d0f85b85c816a9108f1fffcc0ef1cdc
Louisan Tchitoula <t.louisan@outlook.fr>
2025-07-09 16:39:29 +0200

    feat: add postgres module and dbcontext

--- apps/tomato_backend/src/Agribus.Postgres/Persistence/ApplicationDbContextFactory.cs
+++ apps/tomato_backend/src/Agribus.Postgres/Persistence/ApplicationDbContextFactory.cs
@@ -4,16 +4,25 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AgribusDb
 {
     public AgribusDbContext CreateDbContext(string[] args)
     {
-        var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(directoryInfo.FullName)
-            .AddJsonFile("appsettings.json")
-            .Build();
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__Postgres");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(directoryInfo.FullName)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            connectionString = configuration.GetConnectionString("Postgres");
+        }
+
+        if (string.IsNullOrEmpty(connectionString))
+            throw new InvalidOperationException("Could not find connection string");
 
         var builder = new DbContextOptionsBuilder<AgribusDbContext>();
-        builder
-            .UseNpgsql(configuration.GetConnectionString("Postgres")!)
-            .UseSnakeCaseNamingConvention();
+        builder.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
 
         return new AgribusDbContext(builder.Options);
     }
