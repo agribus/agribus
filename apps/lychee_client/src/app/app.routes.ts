5feda2d17588e52849a5948e78fc510638b49cd4
heitzjulien <contact@heitzjulien.fr>
2025-07-09 11:10:56 +0200

    feat(lychee_client): add unsupported platform page with localization, routing, and dynamic language-based assets

--- apps/lychee_client/src/app/app.routes.ts
+++ apps/lychee_client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from "@angular/router";
+import { UnsupportedPlatformComponent } from "@pages/unsupported-platform/unsupported-platform.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "unsupported-platform", pathMatch: "full" },
+  { path: "unsupported-platform", component: UnsupportedPlatformComponent },
+];
