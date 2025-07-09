1d001906abed018750d6609116d2a2b0b1396375
mattisavec2t <mattis.almeidalima@gmail.com>
2025-07-09 10:25:38 +0200

    style(lychee): new prettier config

--- apps/lychee_client/capacitor.config.ts
+++ apps/lychee_client/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.lychee.app',
-  appName: 'lychee',
-  webDir: 'dist/lychee_client/browser',
+  appId: "com.lychee.app",
+  appName: "lychee",
+  webDir: "dist/lychee_client/browser",
 };
 
 export default config;
--- apps/lychee_client/src/app/components/hello-world/hello-world.component.ts
+++ apps/lychee_client/src/app/components/hello-world/hello-world.component.ts
@@ -1,23 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
-import { TuiBadge, TuiStatus } from '@taiga-ui/kit';
-import { PlatformService } from '@services/platform/platform.service';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { TuiButton } from '@taiga-ui/core';
+import { Component, inject, signal } from "@angular/core";
+import { TuiBadge, TuiStatus } from "@taiga-ui/kit";
+import { PlatformService } from "@services/platform/platform.service";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+import { TuiButton } from "@taiga-ui/core";
 
 @Component({
-  selector: 'app-hello-world',
+  selector: "app-hello-world",
   imports: [TuiBadge, TuiStatus, TranslatePipe, TuiButton],
-  templateUrl: './hello-world.component.html',
-  styleUrl: './hello-world.component.scss',
+  templateUrl: "./hello-world.component.html",
+  styleUrl: "./hello-world.component.scss",
 })
 export class HelloWorldComponent {
-  public message = signal<string>('');
+  public message = signal<string>("");
 
   private readonly platformService = inject(PlatformService);
   private readonly translateService = inject(TranslateService);
 
   constructor() {
-    this.message.set(this.platformService.isBrowser() ? 'hello.browser' : 'hello.mobile');
+    this.message.set(this.platformService.isBrowser() ? "hello.browser" : "hello.mobile");
   }
 
   useLanguage(language: string): void {
