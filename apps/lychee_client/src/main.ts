c977ecb1fd1257b5b6ada7d514c426312ac15ba4
heitzjulien <contact@heitzjulien.fr>
2025-07-09 10:19:48 +0200

    refactor(lychee_client): rename BootstrapApp to AppComponent and update related references

--- apps/lychee_client/src/main.ts
+++ apps/lychee_client/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { BootstrapApp } from './app/app.component';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(BootstrapApp, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
