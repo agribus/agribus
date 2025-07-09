a3e3b02a561c760d6d078186377fccb1b8a2466f
mattisavec2t <mattis.almeidalima@gmail.com>
2025-07-09 10:25:38 +0200

    style(lychee): new prettier config

--- apps/lychee_client/src/main.ts
+++ apps/lychee_client/src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { BootstrapApp } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { BootstrapApp } from "./app/app.component";
 
-bootstrapApplication(BootstrapApp, appConfig).catch((err) => console.error(err));
+bootstrapApplication(BootstrapApp, appConfig).catch(err => console.error(err));
