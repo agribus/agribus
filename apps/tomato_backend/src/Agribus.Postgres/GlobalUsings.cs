4f39b98e03efabe05a0591a2987580ad69322552
Louisan Tchitoula <t.louisan@outlook.fr>
2025-07-09 16:39:29 +0200

    feat: add postgres module and dbcontext
    # Conflicts:
    #	docker/compose.preprod.yaml

--- apps/tomato_backend/src/Agribus.Postgres/GlobalUsings.cs
+++ apps/tomato_backend/src/Agribus.Postgres/GlobalUsings.cs
@@ -1,4 +1,3 @@
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Configuration;
