e34d66785e84d3b80d6996d694f8b33fb8ed43ea
heitzjulien <contact@heitzjulien.fr>
2025-07-09 10:19:48 +0200

    refactor(lychee_client): rename BootstrapApp to AppComponent and update related references

--- apps/lychee_client/src/app/app.component.ts
+++ apps/lychee_client/src/app/app.component.ts
@@ -2,18 +2,16 @@ import { TuiRoot } from '@taiga-ui/core';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from '@components/hello-world/hello-world.component';
-import {TranslateModule} from "@ngx-translate/core";
-import {TranslateService} from "@ngx-translate/core";
-
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, HelloWorldComponent, TranslateModule,],
+  imports: [RouterOutlet, TuiRoot, HelloWorldComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class BootstrapApp {
-
+export class AppComponent {
   private readonly translateService = inject(TranslateService);
   constructor() {
     this.translateService.addLangs(['fr', 'en', 'de']);
