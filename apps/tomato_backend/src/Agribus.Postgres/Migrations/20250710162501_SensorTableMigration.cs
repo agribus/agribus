753c2ade66bb8e9cb2807868aaea3379af3f6561
Louisan Tchitoula <t.louisan@outlook.fr>
2025-07-11 17:23:46 +0200

    chore: format

--- apps/tomato_backend/src/Agribus.Postgres/Migrations/20250710162501_SensorTableMigration.cs
+++ apps/tomato_backend/src/Agribus.Postgres/Migrations/20250710162501_SensorTableMigration.cs
@@ -15,27 +15,46 @@ namespace Agribus.Postgres.Migrations
                 name: "sensor",
                 columns: table => new
                 {
-                    id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    id = table.Column<Guid>(
+                        type: "uuid",
+                        nullable: false,
+                        defaultValueSql: "gen_random_uuid()"
+                    ),
                     name = table.Column<string>(type: "text", nullable: false),
                     source_address = table.Column<string>(type: "varchar(50)", nullable: false),
                     sensor_model = table.Column<string>(type: "varchar(50)", nullable: false),
-                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
-                    created_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
-                    last_modified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                    is_active = table.Column<bool>(
+                        type: "boolean",
+                        nullable: false,
+                        defaultValue: true
+                    ),
+                    created_at = table.Column<DateTimeOffset>(
+                        type: "timestamp with time zone",
+                        nullable: false,
+                        defaultValueSql: "NOW()"
+                    ),
+                    last_modified = table.Column<DateTimeOffset>(
+                        type: "timestamp with time zone",
+                        nullable: false,
+                        defaultValueSql: "NOW()"
+                    ),
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("pk_sensor", x => x.id);
-                    table.CheckConstraint("CK_Sensor_Model_IsValid", "sensor_model IN ('RuuviTag', 'RuuviTagPro', 'Unknown')");
+                    table.CheckConstraint(
+                        "CK_Sensor_Model_IsValid",
+                        "sensor_model IN ('RuuviTag', 'RuuviTagPro', 'Unknown')"
+                    );
                 },
-                comment: "Sensors table stores information about sensors used in the system.");
+                comment: "Sensors table stores information about sensors used in the system."
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "sensor");
+            migrationBuilder.DropTable(name: "sensor");
         }
     }
 }
