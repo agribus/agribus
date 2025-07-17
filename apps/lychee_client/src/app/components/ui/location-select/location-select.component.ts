62f43ee359062d70d6e557a483ce2523c6a3aa14
heitzjulien <contact@heitzjulien.fr>
2025-07-17 17:17:29 +0200

    feat(lychee): implement location service and add location-select component

---
+++ apps/lychee_client/src/app/components/ui/location-select/location-select.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-location-select",
+  imports: [],
+  templateUrl: "./location-select.component.html",
+  styleUrl: "./location-select.component.scss",
+})
+export class LocationSelectComponent {}
---
+++ apps/lychee_client/src/app/interfaces/user-location.interface.ts
@@ -0,0 +1,6 @@
+export interface UserLocation {
+  country: string;
+  countryCode: string;
+  latitude: number;
+  longitude: number;
+}
