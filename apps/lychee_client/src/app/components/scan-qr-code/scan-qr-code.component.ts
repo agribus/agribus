ad9d9bef221086bba5eba5a53ab938fef1bade95
heitzjulien <contact@heitzjulien.fr>
2025-07-19 14:42:06 +0200

    refactor(lychee_client): simplify QR code scanner

--- apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
+++ apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { TuiDialogContext } from "@taiga-ui/core";
-import { injectContext } from "@taiga-ui/polymorpheus";
+import { Component, OnInit } from "@angular/core";
 import { BarcodeScanner } from "@capacitor-mlkit/barcode-scanning";
-import { PlatformService } from "@services/platform/platform.service";
 
 @Component({
   selector: "app-scan-qr-code",
@@ -11,34 +8,15 @@ import { PlatformService } from "@services/platform/platform.service";
   styleUrl: "./scan-qr-code.component.scss",
 })
 export class ScanQrCodeComponent implements OnInit {
-  private platformService = inject(PlatformService);
-
-  ngOnInit() {
-    if (this.platformService.isMobile()) {
-      this.startScan();
-    }
-  }
-
-  isScanning = false;
-  error: string | null = null;
-
-  async startScan() {
-    this.isScanning = true;
-    this.error = "coucou";
-
+  async ngOnInit() {
     try {
       const { barcodes } = await BarcodeScanner.scan();
 
       if (barcodes.length > 0 && barcodes[0].rawValue) {
         console.log(barcodes[0].rawValue);
-      } else {
-        this.error = "Aucun QR code détecté.";
       }
     } catch (err) {
-      this.error = "Erreur lors du scan.";
       console.error(err);
-    } finally {
-      this.isScanning = false;
     }
   }
 }
