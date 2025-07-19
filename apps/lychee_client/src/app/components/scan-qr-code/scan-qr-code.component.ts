29fc1c9ffee44d86a762144238b37b91630f2b3c
heitzjulien <contact@heitzjulien.fr>
2025-07-19 11:08:14 +0200

    refactor(lychee_client): simplify QR code scanner component by removing camera permissions logic

--- apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
+++ apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
@@ -1,9 +1,8 @@
-import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
-import { ZXingScannerComponent, ZXingScannerModule } from "@zxing/ngx-scanner";
+import { Component } from "@angular/core";
+import { ZXingScannerModule } from "@zxing/ngx-scanner";
 import { BarcodeFormat } from "@zxing/library";
 import { TuiDialogContext } from "@taiga-ui/core";
 import { injectContext } from "@taiga-ui/polymorpheus";
-import { Camera } from "@capacitor/camera";
 
 @Component({
   selector: "app-scan-qr-code",
@@ -11,25 +10,10 @@ import { Camera } from "@capacitor/camera";
   templateUrl: "./scan-qr-code.component.html",
   styleUrl: "./scan-qr-code.component.scss",
 })
-export class ScanQrCodeComponent implements AfterViewInit {
+export class ScanQrCodeComponent {
   allowedFormats = [BarcodeFormat.QR_CODE];
 
-  public hasPermission = false;
-  @ViewChild(ZXingScannerComponent) scanner!: ZXingScannerComponent;
   public readonly context = injectContext<TuiDialogContext<string>>();
-
-  async ngAfterViewInit() {
-    const permission = await Camera.requestPermissions();
-    this.hasPermission = permission.camera === "granted";
-
-    if (this.hasPermission) {
-      this.scanner.camerasFound.subscribe(async (devices: MediaDeviceInfo[]) => {
-        alert("camera found");
-      });
-    } else {
-      alert("Permission caméra refusée. Impossible de scanner un QR code.");
-    }
-  }
 
   handleQrCodeResult(result: string): void {
     this.context.completeWith(result);
