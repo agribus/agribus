be5c274040f04e5933b2fea9f4e42fa12ac9f59a
heitzjulien <contact@heitzjulien.fr>
2025-07-19 10:53:43 +0200

    feat(lychee_client): request camera permissions in QR code scanner and add @capacitor/camera

--- apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
+++ apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ZXingScannerModule } from "@zxing/ngx-scanner";
 import { BarcodeFormat } from "@zxing/library";
 import { TuiDialogContext } from "@taiga-ui/core";
 import { injectContext } from "@taiga-ui/polymorpheus";
+import { Camera } from "@capacitor/camera";
 
 @Component({
   selector: "app-scan-qr-code",
@@ -10,10 +11,14 @@ import { injectContext } from "@taiga-ui/polymorpheus";
   templateUrl: "./scan-qr-code.component.html",
   styleUrl: "./scan-qr-code.component.scss",
 })
-export class ScanQrCodeComponent {
+export class ScanQrCodeComponent implements OnInit {
   allowedFormats = [BarcodeFormat.QR_CODE];
 
   public readonly context = injectContext<TuiDialogContext<string>>();
+
+  async ngOnInit() {
+    await Camera.requestPermissions();
+  }
 
   handleQrCodeResult(result: string): void {
     this.context.completeWith(result);
