c4554d974e57a8e4dd161de7bf79dc0972a255e2
heitzjulien <contact@heitzjulien.fr>
2025-07-19 12:05:14 +0200

    feat(lychee): integrate `@capacitor-mlkit/barcode-scanning` to enhance QR code scanning

--- apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
+++ apps/lychee_client/src/app/components/scan-qr-code/scan-qr-code.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { ZXingScannerModule } from "@zxing/ngx-scanner";
 import { BarcodeFormat } from "@zxing/library";
 import { TuiDialogContext } from "@taiga-ui/core";
 import { injectContext } from "@taiga-ui/polymorpheus";
+import { BarcodeScanner } from "@capacitor-mlkit/barcode-scanning";
+import { PlatformService } from "@services/platform/platform.service";
 
 @Component({
   selector: "app-scan-qr-code",
@@ -10,12 +12,48 @@ import { injectContext } from "@taiga-ui/polymorpheus";
   templateUrl: "./scan-qr-code.component.html",
   styleUrl: "./scan-qr-code.component.scss",
 })
-export class ScanQrCodeComponent {
+export class ScanQrCodeComponent implements OnInit {
   allowedFormats = [BarcodeFormat.QR_CODE];
 
   public readonly context = injectContext<TuiDialogContext<string>>();
 
   handleQrCodeResult(result: string): void {
     this.context.completeWith(result);
+  }
+
+  private platformService = inject(PlatformService);
+  isDesktop = this.platformService.isBrowser();
+
+  ngOnInit() {
+    if (this.isDesktop) {
+      BarcodeScanner.requestPermissions();
+    }
+
+    if (this.platformService.isMobile()) {
+      this.startScan();
+    }
+  }
+
+  isScanning = false;
+  error: string | null = null;
+
+  async startScan() {
+    this.isScanning = true;
+    this.error = null;
+
+    try {
+      const { barcodes } = await BarcodeScanner.scan();
+
+      if (barcodes.length > 0 && barcodes[0].rawValue) {
+        this.context.completeWith(barcodes[0].rawValue);
+      } else {
+        this.error = "Aucun QR code détecté.";
+      }
+    } catch (err) {
+      this.error = "Erreur lors du scan.";
+      console.error(err);
+    } finally {
+      this.isScanning = false;
+    }
   }
 }
