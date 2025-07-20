8484406d34b649707d831f8de52f647327f3d887
Kobrae-San <djedjegboble.pro@gmail.com>
2025-07-20 16:04:51 +0200

    refactor(app.route.ts): modifying component called for login page
    Calling LoginComponent from pages/login instead of LoginFormComponent from components/login-form

--- apps/lychee_client/src/app/app.routes.ts
+++ apps/lychee_client/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from "@components/dashboard/dashboard.component";
 import { SettingsAccountComponent } from "@components/settings-account/settings-account.component";
 import { GreenhouseFormComponent } from "@components/greenhouse-form/greenhouse-form.component";
 import { SettingsComponent } from "@pages/settings/settings.component";
-import { LoginFormComponent } from "@components/login-form/login-form.component";
+import { LoginComponent } from "@pages/login/login.component";
 import { RegisterFormComponent } from "@components/register-form/register-form.component";
 import { ForgotPasswordComponent } from "@components/forgot-password/forgot-password.component";
 
@@ -17,12 +17,16 @@ export const routes: Routes = [
   { path: "greenhouse-form", component: GreenhouseFormComponent, canActivate: [mobileOnlyGuard] },
   { path: "settings", component: SettingsComponent, canActivate: [mobileOnlyGuard] },
   { path: "settings-account", component: SettingsAccountComponent, canActivate: [mobileOnlyGuard] },
-  { path: "login", component: LoginFormComponent, canActivate: [mobileOnlyGuard] },
   { path: "register", component: RegisterFormComponent, canActivate: [mobileOnlyGuard] },
   { path: "forgot-password", component: ForgotPasswordComponent, canActivate: [mobileOnlyGuard] },
 
   {
     path: "unsupported-platform",
     component: UnsupportedPlatformComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    canActivate: [mobileOnlyGuard],
   },
 ];
