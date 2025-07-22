737a3059bdbd3d651df1a3e9fe3750babfb9cf9f
Louisan Tchitoula <t.louisan@outlook.fr>
2025-07-22 21:34:28 +0200

    fix(crop): add plantingDate prop

--- apps/tomato_backend/src/Agribus.Core/Domain/AggregatesModels/GreenhouseAggregates/Crop.cs
+++ apps/tomato_backend/src/Agribus.Core/Domain/AggregatesModels/GreenhouseAggregates/Crop.cs
@@ -5,4 +5,5 @@ public record Crop
     public required string ScientificName { get; init; }
     public required string CommonName { get; init; }
     public required int Quantity { get; init; }
+    public required DateTime PlantingDate { get; init; }
 }
