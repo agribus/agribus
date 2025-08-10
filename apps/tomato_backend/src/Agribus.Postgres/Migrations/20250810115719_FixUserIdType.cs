8c63e04b4de39044d9d39b51310aed982072dca0
Louisan Tchitoula <t.louisan@outlook.fr>
2025-08-10 13:58:03 +0200

    fix(db): user_id type is varchar 32

---
+++ apps/tomato_backend/src/Agribus.Postgres/Migrations/20250810115719_FixUserIdType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agribus.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserIdType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "greenhouse",
+                type: "varchar(32)",
+                nullable: false,
+                comment: "user table is currently stored in an external database (Clerk)",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldComment: "user table is currently stored in an external database (Clerk)"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "greenhouse",
+                type: "text",
+                nullable: false,
+                comment: "user table is currently stored in an external database (Clerk)",
+                oldClrType: typeof(string),
+                oldType: "varchar(32)",
+                oldComment: "user table is currently stored in an external database (Clerk)"
+            );
+        }
+    }
+}
