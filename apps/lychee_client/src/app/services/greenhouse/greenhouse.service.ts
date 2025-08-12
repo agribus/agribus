fdd58addd95ace7b53d89fd0f9abc211289079a1
heitzjulien <contact@heitzjulien.fr>
2025-08-12 10:24:12 +0200

    feat(greenhouse): add greenhouse creation feature and integrate with form submission

--- apps/lychee_client/src/app/services/greenhouse/greenhouse.service.ts
+++ apps/lychee_client/src/app/services/greenhouse/greenhouse.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Greenhouse } from "@interfaces/greenhouse.interface";
+import { environment } from "@environment/environment";
+import { Crop } from "@interfaces/crop.interface";
+import { Sensor } from "@interfaces/sensor.interface";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root",
 })
 export class GreenhouseService {
+  private readonly http = inject(HttpClient);
+
   protected readonly greenhouses: Greenhouse[] = [
     { id: 42, name: "Serre 1" },
     { id: 237, name: "Serre 2" },
@@ -13,5 +19,21 @@ export class GreenhouseService {
 
   getGreenhouses(): Greenhouse[] {
     return this.greenhouses;
+  }
+
+  public createGreenhouse(
+    name: string,
+    city: string,
+    country: string,
+    crops: Crop[],
+    sensors: Sensor[]
+  ) {
+    return this.http.post(environment.apiUrl + "/greenhouses", {
+      name: name,
+      city: city,
+      country: country,
+      crops: crops,
+      sensors: sensors,
+    });
   }
 }
