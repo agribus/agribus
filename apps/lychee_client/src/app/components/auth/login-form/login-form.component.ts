737bf2e3ef3df0cf3bdb5cdf7f968c9adaeffe39
Kobrae-San <djedjegboble.pro@gmail.com>
2025-08-13 18:16:07 +0200

    feat(login-form): add error handling and notifications for invalid login credentials

--- apps/lychee_client/src/app/components/auth/login-form/login-form.component.ts
+++ apps/lychee_client/src/app/components/auth/login-form/login-form.component.ts
@@ -1,9 +1,15 @@
 import { AsyncPipe } from "@angular/common";
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { TuiButton, TuiError, TuiTextfield } from "@taiga-ui/core";
+import {
+  TuiAlertService,
+  TuiButton,
+  TuiError,
+  TuiNotification,
+  TuiTextfield,
+} from "@taiga-ui/core";
 import { TuiFieldErrorPipe, tuiValidationErrorsProvider } from "@taiga-ui/kit";
 import { AuthLogin, AuthResponse } from "@interfaces/auth.interface";
 import { AuthService } from "@services/auth/auth.service";
@@ -18,14 +24,16 @@ import { AuthService } from "@services/auth/auth.service";
     TuiFieldErrorPipe,
     TuiTextfield,
     TranslatePipe,
+    TuiNotification,
   ],
   templateUrl: "./login-form.component.html",
   styleUrl: "./login-form.component.scss",
-  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
     tuiValidationErrorsProvider({
       required: "Champ requis",
       email: "Veuillez entrez une adresse mail valide",
+      minlength: "Le mot de passe doit faire au moins 8 caractères",
+      maxlength: "Le mot de passe ne doit pas dépasser 32 caractères"
     }),
   ],
 })
@@ -35,9 +43,14 @@ export class LoginFormComponent {
   private router = inject(Router);
   protected readonly form = new FormGroup({
     email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", Validators.required),
+    password: new FormControl("", [Validators.required, Validators.minLength(8),
+      Validators.maxLength(32)]),
   });
   private readonly lang = localStorage.getItem("lang") || "fr";
+  private readonly alerts = inject(TuiAlertService);
+
+  public messageError: string = "";
+  public isError: boolean = false;
 
   constructor() {
     this.translateService.use(this.lang);
@@ -57,7 +70,19 @@ export class LoginFormComponent {
             this.router.navigate(["/home"]);
           }
         },
-        error: (error: Error) => console.log(error),
+        error: error => {
+          if (error.status === 400) {
+            this.isError = true;
+            this.messageError = this.translateService.instant("components.ui.alert.errorCreds");
+          } else {
+            this.alerts
+              .open(error.message, {
+                label: this.translateService.instant("components.ui.alert.error"),
+                appearance: "error",
+              })
+              .subscribe();
+          }
+        },
       });
     }
   }
