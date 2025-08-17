ec783ab1dce7edf42f07fe71582cdd732cfb828f
heitzjulien <contact@heitzjulien.fr>
2025-08-17 11:18:27 +0200

    refactor(client): update tsconfig paths to use `@routes` alias and adjust imports accordingly

--- apps/lychee_client/src/app/app.routes.ts
+++ apps/lychee_client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
-import { MAIN_ROUTES } from "./routes/main.routes";
-import { SETTINGS_ROUTES } from "./routes/settings.routes";
-import { AUTH_ROUTES } from "./routes/auth.routes";
-import { ERROR_ROUTES } from "./routes/error.routes";
+import { MAIN_ROUTES } from "@routes/main.routes";
+import { SETTINGS_ROUTES } from "@routes/settings.routes";
+import { AUTH_ROUTES } from "@routes/auth.routes";
+import { ERROR_ROUTES } from "@routes/error.routes";
 
 export const routes: Routes = [
   {
