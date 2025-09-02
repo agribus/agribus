7f04e9bd20c4e7c0d06689e3cc48e2fb405dc507
Louisan Tchitoula <t.louisan@outlook.fr>
2025-09-02 18:45:07 +0200

    fix(alerts): add missing migrations for ranges properties

--- apps/tomato_backend/src/Agribus.Postgres/Migrations/20250902164323_AddAlertValueRanges.Designer.cs
+++ apps/tomato_backend/src/Agribus.Postgres/Migrations/20250902164323_AddAlertValueRanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Agribus.Postgres.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Agribus.Postgres.Migrations
 {
     [DbContext(typeof(AgribusDbContext))]
-    partial class AgribusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250902164323_AddAlertValueRanges")]
+    partial class AddAlertValueRanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ apps/tomato_backend/src/Agribus.Postgres/Migrations/20250902164323_AddAlertValueRanges.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agribus.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlertValueRanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "threshold_value",
+                table: "alert",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision"
+            );
+
+            migrationBuilder.AlterColumn<double>(
+                name: "range_min_value",
+                table: "alert",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision"
+            );
+
+            migrationBuilder.AlterColumn<double>(
+                name: "range_max_value",
+                table: "alert",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "threshold_value",
+                table: "alert",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true
+            );
+
+            migrationBuilder.AlterColumn<double>(
+                name: "range_min_value",
+                table: "alert",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true
+            );
+
+            migrationBuilder.AlterColumn<double>(
+                name: "range_max_value",
+                table: "alert",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true
+            );
+        }
+    }
+}
