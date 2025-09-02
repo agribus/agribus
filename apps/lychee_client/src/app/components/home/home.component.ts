aed4a3d2a5f38cea36863d482c1ce6141fd3db2a
mattisavec2t <mattis.almeidalima@gmail.com>
2025-09-02 14:17:50 +0200

    feat(home): add button when no greenhouse

--- apps/lychee_client/src/app/components/home/home.component.ts
+++ apps/lychee_client/src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { GreenhouseSceneComponent } from "@components/greenhouse-scene/greenhouse-scene.component";
+import { GreenhouseService } from "@services/greenhouse/greenhouse.service";
+import { TuiButton } from "@taiga-ui/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-home",
-  imports: [GreenhouseSceneComponent],
+  imports: [GreenhouseSceneComponent, TuiButton],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private readonly greenhouseService = inject(GreenhouseService);
+  private readonly router = inject(Router);
+
+  get userHaveGreenhouse(): boolean {
+    return this.greenhouseService.greenhouses().length > 0;
+  }
+
+  navigateToGreenhouseForm() {
+    this.router.navigate(["/greenhouse/create"]);
+  }
+}
